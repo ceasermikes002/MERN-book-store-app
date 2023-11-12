a350d45dd71a52241f1a7efa40b21ece223c9a95
chima <chimaemekaiheonu@gmail.com>
2023-11-12 12:33:06 +0100

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+
+export const mongoDBURL= 'mongodb+srv://bookdb:moby@bookdb.daoqtjt.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import ShowBooks from './pages/ShowBooks'
+import EditBooks from './pages/EditBooks'
+import DeleteBooks from './pages/DeleteBooks'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/books/create" element={<CreateBooks/>}/>
+      <Route path="/books/details/:id" element={<ShowBooks/>}/>
+      <Route path="/books/edit/:id" element={<EditBooks />}/>
+      <Route path="/books/delete/:id" element={<DeleteBooks />}/>
+    </Routes>
+  )
+}
+
+export default App
